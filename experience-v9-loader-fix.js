47e233d209000c504a1b21c8da40f014b044de6b
lavidadeuncreativo <lavidadeuncreativo@gmail.com>
2026-07-18 14:35:59 -0600

    Remove obsolete layered frontend runtimes

--- experience-v9-loader-fix.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-  'use strict';
-  window.setTimeout(()=>{
-    const loader=document.getElementById('loader');
-    if(loader&&!loader.classList.contains('v9-done'))loader.classList.remove('hide');
-  },1080);
-})();
